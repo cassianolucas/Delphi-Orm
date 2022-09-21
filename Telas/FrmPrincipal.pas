3e90c084a4d342c8e6785d25dd94fdb7160f366e
cassianolucas <31084413+cassianolucas@users.noreply.github.com>
2022-09-21 08:59:39 -0300

    Correção bug
    Adicionado "Uses"

--- Telas/FrmPrincipal.pas
+++ Telas/FrmPrincipal.pas
@@ -2,6 +2,7 @@ unit FrmPrincipal;
 
 interface
 
+uses
   {$IF RTLVersion > 21.0}
     Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
     Vcl.Controls, Vcl.Forms, Vcl.Dialogs, System.Generics.Collections,
