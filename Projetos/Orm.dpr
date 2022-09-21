95ad9f4f3f87057c36bf84d53fb77a77b1cce65b
cassianolucas <31084413+cassianolucas@users.noreply.github.com>
2022-09-21 08:43:41 -0300

    Alterações para rodar em versões anteriores

--- Projetos/Orm.dpr
+++ Projetos/Orm.dpr
@@ -1,12 +1,17 @@
 program Orm;
 
 uses
-  Vcl.Forms,
+  {$IF RTLVersion > 21.0}
+    Vcl.Forms,
+  {$ELSE}
+    Forms,
+  {$IFEND}
   FrmPrincipal in '..\Telas\FrmPrincipal.pas' {Form1},
   uAtributoBancoModel in '..\Classes\Modelos\uAtributoBancoModel.pas',
   uBancoUtils in '..\Classes\Utils\uBancoUtils.pas',
   uBaseModel in '..\Classes\Modelos\uBaseModel.pas',
-  uClassesBancoModel in '..\Classes\Modelos\uClassesBancoModel.pas';
+  uClassesBancoModel in '..\Classes\Modelos\uClassesBancoModel.pas',
+  uExcecoesBanco in '..\Classes\Comum\uExcecoesBanco.pas';  
 
 {$R *.res}
 
